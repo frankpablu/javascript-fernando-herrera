f832e1560f3f53cd2e36f0eaade5fcf9c168ffe3
franklinspablu <franklinspablu@gmail.com>
2023-08-05 17:26:50 -0500

    first commit

---
+++ 4. Comprendiendo al consola de JavaScript/app.js
@@ -0,0 +1,13 @@
+
+
+var a = 1;
+var b = "Frank";
+
+
+console.log(a);
+console.warn(a);
+console.error(a);
+console.info(a);
+
+
+console.log(a+a);
---
+++ 8. Notación de punto y corchetes/app.js
@@ -0,0 +1,24 @@
+
+var persona = {
+    nombre: "Juana",
+    apellido: "Herrera",
+    edad: 25,
+    direccion: {
+        pais: "Costa Rica",
+        ciudad: "San José",
+        edificio:{
+            nombre: "Edificio principal",
+            telefono: "2222-3333"
+        }
+    }
+};
+
+
+var campo = "edad";
+
+console.log(persona[campo]);
+
+
+console.log(persona.edad2);
+
+
