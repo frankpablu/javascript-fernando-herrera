5f3505df81eee7390f328b3c31d1688c2d37b216
franklinspablu <franklinspablu@gmail.com>
2023-08-06 16:56:12 -0500

    El retorno de las funciones

---
+++ 10. El retorno de las funciones/app.js
@@ -0,0 +1,63 @@
+
+
+
+
+function obtenerAleatorio(){
+
+    
+    return Math.random();
+
+}
+
+
+function obtenerNombre(){
+    return "Juan";
+
+}
+
+function esMayor05(){
+    if (obtenerAleatorio() > 0.5) {
+        return true;
+    }else{
+        return false;
+    }
+    }
+
+    if (esMayor05()) {
+        console.log("Es mayor a 0.5");
+    }else{
+        console.log("Es menor a 0.5");
+    }
+
+    function crearPersona(nombre, apellido){
+        
+        return {
+            nombre : nombre,
+            apellido: apellido
+
+        }
+    }
+    
+    var persona = crearPersona("Maria","Paz");
+    
+
+
+    function creaFuncion(){
+
+        return function(nombre){
+            console.log("Me creo " + nombre);
+
+            return function(){
+                console.log("Segunda funcion");
+            }
+        }
+
+       
+    }
+
+
+
+    var nuevaFuncion = creaFuncion();
+    
+    var segundaFuncion = nuevaFuncion(persona.nombre);
+    segundaFuncion();
