af82faec34a88e2986cbff1e3add67971b733401
franklinspablu <franklinspablu@gmail.com>
2023-08-08 00:52:48 -0500

    Funciones de primera clase

---
+++ 11. Funciones de primera clase/app.js
@@ -0,0 +1,17 @@
+
+function a(){
+
+    console.log("Función a");
+}
+
+a();
+
+a.apply = "María";
+a.direccion = {
+    país: "Costa Rica",
+    ciudad: "San José",
+    edificio:{
+        piso:"8vo",
+        nombre: "Edicio Principal"
+    }
+}
